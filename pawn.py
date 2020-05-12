109581404ef98d0fc6614dd17c3f36fff1915503
Arjit Tiwari <arjittiwari@gmail.com>
2020-05-12 21:11:40 +0530

    Added default position code for all pieces

--- pawn.py
+++ pawn.py
@@ -1,2 +1,32 @@
-class Pawn:
-    pass
+from color import *
+from piece import *
+class Pawn(Piece):
+    __symbol = "P"
+    def __init__(self,chess,player,position):
+        self.chess = chess
+        self.player = player
+        if player == "black":
+            self.color = Color.RED
+        else:
+            self.color = Color.YELLOW
+        self.position = position
+        self.setPosition(self.position)
+    
+    def move(self,position):
+        pass
+    def kill(self):
+        pass
+    def getColor(self):
+        return self.color
+    def getSymbol(self):
+        return Pawn.__symbol
+    def getPlayerName(self):
+        return self.player.name
+    def getPosition(self):
+        return self.position
+    def setPlayerName(self,name):
+        self.player.name = name
+    def setPosition(self,position):
+        self.chess.setPosition(self)
+    def possibleMove(self):
+        pass
