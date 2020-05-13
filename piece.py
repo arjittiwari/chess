8f497552f3d720206f5c7f09b0520fd9b907ef24
Arjit Tiwari <arjittiwari@gmail.com>
2020-05-13 16:58:26 +0530

    Adding piece.py file

---
+++ piece.py
@@ -0,0 +1,16 @@
+class Piece:
+    def move(self,position):
+        pass
+    def kill(self):
+        pass
+    def getPlayerName(self):
+        pass
+    def getPosition(self):
+        pass
+    def setPlayerName(self):
+        pass
+    def setPosition(self):
+        pass
+    def possibleMove(self):
+        pass
+    
