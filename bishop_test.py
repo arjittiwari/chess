ab17297eff4e2c9c065689284ad78d86d0e2c7ee
Ashish Nayak <anayakiitd@gmail.com>
2020-05-13 13:42:46 +0530

    testing

---
+++ bishop_test.py
@@ -0,0 +1,32 @@
+from color import *
+from piece import *
+class Bishop(Piece):
+      __symbol = "B"
+      def __init__(self,chess,player,position):
+            self.chess = chess
+            self.player = player
+            if player == "black":
+                  self.color = Color.RED
+            else:
+                  self.color = Color.YELLOW
+            self.position = position
+            self.setPosition(self.position)
+      
+      def move(self,position):
+            pass
+      def kill(self):
+            pass
+      def getColor(self):
+            return self.color
+      def getSymbol(self):
+            return Bishop.__symbol
+      def getPlayerName(self):
+            return self.player.name
+      def getPosition(self):
+            return self.position
+      def setPlayerName(self,name):
+            self.player.name = name
+      def setPosition(self,position):
+            self.chess.setPosition(self)
+      def possibleMove(self):
+            pass
